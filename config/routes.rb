6b50a1149644f3f478636d9f4022763ebacf1280
kei-i-wa <kei.kumarg@gmail.com>
2021-07-09 14:08:23 +0000

    メール送信機能ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get '/search', to: 'searches#search'
   resources :groups do
     get "join"=>"groups#join"
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
   
 end
